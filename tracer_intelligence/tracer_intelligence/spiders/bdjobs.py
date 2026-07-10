8df3a9c102f5b0d05686ed728686a508d4c0911e
Wakibul Zaman Nithor <wkblzmn@gmail.com>
2026-07-10 18:10:54 +0600

    map bdjobs category IDs to names

--- tracer_intelligence/tracer_intelligence/spiders/bdjobs.py
+++ tracer_intelligence/tracer_intelligence/spiders/bdjobs.py
@@ -2,10 +2,77 @@ import scrapy
 import json
 from tracer_intelligence.items import JobPostingItem
 
+CATEGORIES = {
+    -11: "Other Special Skilled Jobs",
+    -10: "Others",
+    1: "Accounting/Finance",
+    2: "Bank/Non-Bank Fin. Institution",
+    3: "Supply Chain/Procurement",
+    4: "Education/Training",
+    5: "Engineer/Architect",
+    6: "Garments/Textile",
+    7: "General Management/Admin",
+    8: "IT/Telecommunication",
+    9: "Marketing/Sales",
+    10: "Media/Advertisement/Event Mgt.",
+    11: "Healthcare/Medical",
+    12: "NGO/Development",
+    13: "Research/Consultancy",
+    14: "Receptionist/PS",
+    15: "Data Entry/Operator/BPO",
+    16: "Customer Service/Call Centre",
+    17: "HR/Org. Development",
+    18: "Design/Creative",
+    19: "Production/Operation",
+    20: "Hospitality/Travel/Tourism",
+    21: "Beauty Care/Health & Fitness",
+    22: "Law/Legal",
+    23: "Electrician/Construction/Repair",
+    24: "Security/Support Service",
+    25: "Driving/Motor Technician",
+    26: "Agro (Plant/Animal/Fisheries)",
+    27: "Commercial",
+    28: "Company Secretary/Regulatory affairs",
+    29: "Pharmaceutical",
+    61: "Data Entry/Computer Operator",
+    62: "Mechanic/Technician",
+    63: "Nurse",
+    64: "Waiter/Waitress",
+    65: "Pathologist/Lab Assistant",
+    66: "Electrician/Electronics Technician",
+    67: "Driver",
+    68: "Chef/Cook",
+    69: "Housekeeper",
+    70: "Security Guard",
+    71: "Graphic Designer",
+    72: "Welder",
+    73: "Plumber/Pipe fitting",
+    74: "Sewing machine operator",
+    75: "Mason/Construction worker",
+    76: "CAD Operator",
+    77: "Delivery Man",
+    78: "Garments technician/Machine operator",
+    79: "Peon",
+    80: "Cleaner",
+    81: "Gardener",
+    82: "Carpenter",
+    83: "Showroom Assistant/Salesman",
+    84: "Sales Representative (SR)",
+    85: "Imam/Khatib/Muezzin",
+    86: "Gym/Fitness Trainer",
+    87: "Interpreter",
+    88: "Beautician/Salon worker",
+    89: "Fire Safety/Firefighter",
+    90: "Boiler Operator",
+    91: "Caregiver/Nanny",
+    92: "Physiotherapist",
+}
+
 API_URL = (
     "https://api.bdjobs.com/Jobs/api/JobSearch/GetJobSearch"
     "?pg={page}&rpp=50&isPro=0&ToggleJobs=true&isFresher=false"
 )
+
 
 class BdjobsSpider(scrapy.Spider):
     name = "bdjobs"
@@ -30,7 +97,10 @@ class BdjobsSpider(scrapy.Spider):
             item["title"]       = job.get("jobTitle", "")
             item["company"]     = job.get("companyName", "")
             item["location"]    = job.get("location", "")
-            item["category"]    = str(job.get("Cat_id", ""))
+
+            cat_id = job.get("Cat_id", 0)
+            item["category"]    = CATEGORIES.get(cat_id, str(cat_id))
+
             item["description"] = job.get("jobDescription", "")
 
             salary = job.get("Salary", {})
@@ -44,14 +114,14 @@ class BdjobsSpider(scrapy.Spider):
                 item["salary_max"] = None
 
             raw_deadline = job.get("deadlineDB", "")
-            item["deadline"] = raw_deadline[:10] if raw_deadline else None
+            item["deadline"]    = raw_deadline[:10] if raw_deadline else None
 
             raw_posted = job.get("publishDate", "")
-            item["posted_at"] = raw_posted[:10] if raw_posted else None
+            item["posted_at"]   = raw_posted[:10] if raw_posted else None
 
             yield item
 
         yield scrapy.Request(
             url=API_URL.format(page=page + 1),
-            callback=self.parse
+            callback=self.parse,
         )
