fa10024e66a46669470658e053f6be340f3551f0
Wakibul Zaman Nithor <wkblzmn@gmail.com>
2026-07-19 13:32:45 +0600

    Remove unused hardcoded Shomvob token

--- tracer_intelligence/shomvob.py
+++ tracer_intelligence/shomvob.py
@@ -6,8 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlNob212b2JUZWNoQVBJVXNlciIsImlhdCI6MTY1OTg5NTcwOH0.IOdKen62ye0N9WljM_cj3Xffmjs3dXUqoJRZ_1ezd4Q"
-API_URL = "https://backend-api.shomvob.co/api/v2/jobpost/get-active-job-list-guest"
+
 
 CATEGORY_MAP = {
     "Sales/Marketing": "Marketing/Sales",
